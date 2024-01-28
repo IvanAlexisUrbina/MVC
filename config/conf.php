37ccac279c00edd5d2ae405d87bb60939e6da1d0
IvanAlexisUrbina <alexis-crokis@hotmial.com>
2024-01-28 17:24:47 -0500

    [init - uni]

--- config/conf.php
+++ config/conf.php
@@ -1,10 +1,10 @@
 <?php
 
     //BD
-    $host="localhost";
-    $username="root";
-    $password="";
-    $dbname="mvc";
+    $host="srv1264.hstgr.io";
+    $username="u638789243_bugivan";
+    $password="Bugivan19+";
+    $dbname="u638789243_uni";
     $port="3306";
     $charset ='utf8mb4'; 
     
